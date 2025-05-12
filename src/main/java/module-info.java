34b1262bfad159d5e787b049ed3cc6cbbfdb73f6
Thiago <thiagodacostaalves35@gmail.com>
2025-05-12 12:09:46 -0300

    Teste de conexão com o  banco

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -10,6 +10,7 @@ module org.example.delta_pdv {
     requires org.kordamp.bootstrapfx.core;
     requires eu.hansolo.tilesfx;
     requires com.almasb.fxgl.all;
+    requires java.sql;
 
     opens org.example.delta_pdv to javafx.fxml;
     exports org.example.delta_pdv;
--- src/main/java/org/example/delta_pdv/repository/DB.java
+++ src/main/java/org/example/delta_pdv/repository/DB.java
@@ -1,2 +1,60 @@
-package org.example.delta_pdv.repository;public class DB {
+package org.example.delta_pdv.repository;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+import java.sql.*;
+
+public class DB {
+
+    private static Connection conn;
+
+    private static Connection getConn() {
+        if (conn != null) {
+            return conn;
+        }
+        try {
+            Properties props = loadProperties();
+            String URL = props.getProperty("url");
+            String USER = props.getProperty("user");
+            String PASSWORD = props.getProperty("password");
+            return conn = DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (SQLException exception) {
+            throw new RuntimeException("Não foi possivel conectar ao banco de dados" + exception);
+        }
+    }
+
+    private static Properties loadProperties() {
+        try(FileInputStream fs = new FileInputStream("db.properties")) {
+            Properties props = new Properties();
+             props.load(fs);
+             return props;
+        }catch (IOException exception) {
+            throw new RuntimeException("Erro ao carregar arquivo!!: " + exception);
+        }
+    }
+
+    private void closeStatement(PreparedStatement ps) {
+        if (ps != null) {
+            try {
+                ps.close();
+            }catch (SQLException exception) {
+                throw new RuntimeException("Erro ao fechar o PrepareStatement" + exception);
+            }
+        }
+    }
+
+    private void closeResultSet(ResultSet rs) {
+        if (rs != null) {
+            try {
+                rs.close();
+            } catch (SQLException exception) {
+            throw new RuntimeException("Erro ao fechar o resultSet");
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Connection conn = getConn();
+    }
 }
